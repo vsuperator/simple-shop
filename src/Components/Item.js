0e67da16ab9d7a4c2a86fb4fef5f23e46bce80f8
vsuperator <roma9209@gmail.com>
2015-07-26 16:09:25 +0300

    styles

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -45,13 +45,15 @@ var Item = React.createClass({
                     <div className="product-title">
                         <a href="#">{_str.truncate(product.title, 20)}</a>
                     </div>
-                    <span className="product-description">{_str.truncate(product.description, 70)}</span>
+
+                    <div className="product-description">{_str.truncate(product.description, 70)}</div>
+
                     <div className="item-select-container">
                         <input type="number" defaultValue="1" value={valueInSelect}
                             min="0" max={balance} onChange={this.handleChange}/>
                         <div className="price-container">
-                            <span className="produc-price">{product.price}</span>
-                            <span className={balance == 0 ? 'disable' : "add-to-basket"}
+                            <span className="produc-price btn">{product.price}</span>
+                            <span className={balance == 0 ? 'disable' : "add-to-basket btn"}
                                 onClick={this.addTobasket}>
                                 A
                             </span>
